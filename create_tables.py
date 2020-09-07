5edda92ec8b7a8b1d6fd6ebfdcdf6391841b4315
Nasseredine Bajwa <fr.bajwa.nasseredine@gmail.com>
2020-09-07 14:45:59 +0200

    docs: Add docstring to create_table.py

--- create_tables.py
+++ create_tables.py
@@ -4,18 +4,31 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    Drops each table using the queries in `drop_table_queries` list.
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    Creates each table using the queries in `create_table_queries` list.
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    - Parses the `dwh.cfg` configuration file.
+    - Connects to the Redshift cluster.
+    - Drops the tables (see :func:`~create_tables.drop_tables`).
+    - Creates the tables (see :func:`~create_tables.create_tables`).
+    - Closes the connection to the Redshift cluster.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
